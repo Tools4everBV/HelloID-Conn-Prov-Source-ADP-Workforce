ef917f68838e969482921c9ca5ae99dcd7609d33
Jeroen <72070984+JeroenBl@users.noreply.github.com>
2020-10-06 12:46:58 +0200

    Update persons.ps1

--- persons.ps1
+++ persons.ps1
@@ -1,3 +1,9 @@
+#####################################################
+# HelloID-Conn-Prov-SOURCE-ADP-Workforce-Persons
+# 
+# Version: 1.0.0
+#####################################################
+
 #Region External functions
 function Get-ADPWorkers {
     <#
@@ -19,17 +25,20 @@ function Get-ADPWorkers {
     .PARAMETER ProxyServer
     The URL (or IP Address) to the ProxyServer in the network. Leave empty if no ProxyServer is being used
 
-    .PARAMETER UsePaging
-    Retrieve the results using pagination
+    .PARAMETER SecurityProtocol
+    The encprytion protocol used when sending data across the network. The default is set to TLS1.2
     #>
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory)]
         [String]
         $CientID,
 
+        [Parameter(Mandatory)]
         [SecureString]
         $ClientSecret,
 
+        [Parameter(Mandatory)]
         [X509Certificate]
         $Certifcate,
 
@@ -38,51 +47,39 @@ function Get-ADPWorkers {
         [String]
         $ProxyServer,
 
-        [Bool]
-        $UsePaging
+        [System.Net.SecurityProtocolType]
+        $SecurityProtocol
     )
 
+    $baseUri = [System.Uri]'https://api.dex.adp.com'
+
     try {
-        #$accessToken = Get-ADPAccessToken -CientID $ClientID -ClientSecret $ClientSecret -Certifcate $Certifcate
-    } catch {
+        $accessToken = Get-ADPAccessToken -CientID $ClientID -ClientSecret $ClientSecret -Certifcate $Certifcate
+    } catch [System.Net.WebException] {
         $webEx = $PSItem
         $errorObj = ($($webEx.ErrorDetails.Message) | ConvertFrom-Json).response
-        $splatErrorRecordParams = @{
-            ErrorMessage = "Could not retrieve ADP AccessToken. Error: '$($errorObj.applicationCode.message)' Code: '$($errorObj.applicationCode.code)'" 
-            ExceptionType = 'System.Net.WebException'
-        }
-        $errorRecord = New-ErrorRecord @splatErrorRecordParams
-        $PSCmdlet.WriteError($errorRecord)
+        $PSCmdlet.WriteWarning("Could not retrieve ADP AccessToken. Error: '$($errorObj.applicationCode.message)' Code: '$($errorObj.applicationCode.code)'")
+    } catch [System.Exception] {
+        $ex = $PSItem
+        $PSCmdlet.WriteWarning("Could not retrieve ADP AccessToken. Error: '$($ex.Message)'")
     }
 
     try {
-        $baseUri = 'https://test-api.adp.com'
-
         $splatADPRestMethodParams = @{
-            BaseUri = "$baseUri/hr/v2/worker-demographics"
-            HttpMethod = 'GET'
+            Uri = "$($baseUri)hr/v2/worker-demographics"
+            Method = 'GET'
             AccessToken = $accessToken
             ProxyServer = $ProxyServer
-            UsePaging = $UsePaging
+            SecurityProtocol = $SecurityProtocol
         }
-        Invoke-ADPRestMethod @splatADPRestMethodParams | ConvertTo-HelloIDPerson
-    } catch [System.Net.WebException]{
+        Invoke-ADPRestMethod @splatADPRestMethodParams | ConvertTo-RawDataPersonObject | ConvertTo-Json -Depth 100
+    } catch [System.Net.WebException] {
         $webEx = $PSItem
         $errorObj = ($($webEx.ErrorDetails.Message) | ConvertFrom-Json).response
-        $splatErrorRecordParams = @{
-            ErrorMessage = "Could not retrieve ADP Workers. Error: '$($errorObj.applicationCode.message)' Code: '$($errorObj.applicationCode.code)'" 
-            ExceptionType = 'System.Net.WebException'
-        }
-        $errorRecord = New-ErrorRecord @splatErrorRecordParams
-        $PSCmdlet.WriteError($errorRecord)
+        $PSCmdlet.WriteError("Could not retrieve ADP Workers. Error: '$($errorObj.applicationCode.message)' Code: '$($errorObj.applicationCode.code)'")
     } catch [System.Exception] {
         $ex = $PSItem
-        $splatErrorRecordParams = @{
-            ErrorMessage = "Could not retrieve ADP Workers. Error: '$($ex.Exception.Message)'"
-            ExceptionType = 'System.Exception'
-        }
-        $errorRecord = New-ErrorRecord @splatErrorRecordParams
-        $PSCmdlet.WriteError($errorRecord)
+        $PSCmdlet.WriteWarning("Could not retrieve ADP Workers. Error: '$($ex.Message)'")
     }
 }
 #EndRegion
@@ -116,17 +113,23 @@ function Get-ADPAccessToken {
     #>
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory)]
         [String]
         $CientID,
 
+        [Parameter(Mandatory)]
         [SecureString]
         $ClientSecret,
 
+        [Parameter(Mandatory)]
         [X509Certificate]
         $Certifcate
     )
 
-    $authorization = "$($CientID):$($ClientSecret)"
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ClientSecret)
+    $clientSecretString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+    
+    $authorization = "$($CientID):$($clientSecretString)"
     $base64String = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($authorization))
 
     $headers = @{
@@ -142,7 +145,7 @@ function Get-ADPAccessToken {
             Method = 'POST'
             Headers = $headers
         }
-        Invoke-RestMethod @splatRestMethodParameters 
+        Invoke-RestMethod @splatRestMethodParameters
     } catch {
         $PSCmdlet.ThrowTerminatingError($PSItem)
     }
@@ -156,10 +159,10 @@ function Invoke-ADPRestMethod {
     .DESCRIPTION
     Sends and receives data to and from the ADP API's
 
-    .PARAMETER BaseUri
+    .PARAMETER Uri
     The BaseUri to the ADP Workforce environment
 
-    .PARAMETER HttpMethod
+    .PARAMETER Method
     The CRUD operation for the request. Valid HttpMethods inlcude: GET and POST
 
     .PARAMETER AccessToken
@@ -168,20 +171,20 @@ function Invoke-ADPRestMethod {
     .PARAMETER ProxyServer
     The URL (or IP Address) to the ProxyServer in the network. Leave empty if no ProxyServer is being used
 
-    .PARAMETER UsePaging
-    Retrieve the results using pagination
+    .PARAMETER SecurityProtocol
+    The encprytion protocol used when sending data across the network. The default is set to TLS1.2
     #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [String]
-        $BaseUri,
+        $Uri,
 
         [Parameter(Mandatory)]
         [String]
-        $HttpMethod,
+        $Method,
 
-        #[Parameter(Mandatory)]
+        [Parameter(Mandatory)]
         [String]
         $AccessToken,
 
@@ -190,14 +193,14 @@ function Invoke-ADPRestMethod {
         [String]
         $ProxyServer,
 
-        [Bool]
-        $UsePaging
+        [System.Net.SecurityProtocolType]
+        $SecurityProtocol
     )
 
     $headers = @{
         "grant_type" = "client_credentials&scope=api"
         "Content-Type" = "application/json"
-        #"Authorization" = "Bearer $AccessToken"
+        "Authorization" = "Bearer $AccessToken"
     }
 
     if ([string]::IsNullOrEmpty($ProxyServer)){
@@ -207,70 +210,28 @@ function Invoke-ADPRestMethod {
     }
 
     try {
-        if ($script:SecurityProtocolUseTls12) {
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-        }
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::$SecurityProtocol
 
         $splatRestMethodParameters = @{
-            Uri = $BaseUri
-            Method = $HttpMethod
+            Uri = $Uri
+            Method = $Method
             Headers = $headers
             Proxy = $proxy
             UseBasicParsing = $true
         }
-        Invoke-RestMethod @splatRestMethodParameters 
-                
+        Invoke-RestMethod @splatRestMethodParameters
     } catch {
         $PSCmdlet.ThrowTerminatingError($PSItem)
     }
 }
 
-function New-ErrorRecord {
+function ConvertTo-RawDataPersonObject {
     <#
     .SYNOPSIS
-    Creates a new Automation.ErrorRecord object
+    Converts the worker objects to a raw data person object
 
     .DESCRIPTION
-    Creates a new custom Automation.ErrorRecord object
-
-    .PARAMETER ErrorMessage
-    The ErrorMessage. Typically this is the error coming back from the API
-
-    .PARAMETER ErrorCode
-    The ErrorCode. Typically this is the errorcode coming back from the API
-
-    .PARAMETER ExceptionType
-    The ExceptionType. Like; System.Net.WebException
-    #>
-    [OutputType('System.Management.Automation.ErrorRecord')]
-    [CmdletBinding()]
-    param (
-        [String]
-        $ErrorMessage,
-
-        [String]
-        $ErrorCode,
-
-        [String]
-        $ExceptionType
-    )
-
-    $exception = New-Object $ExceptionType($ErrorMessage)
-    New-Object System.Management.Automation.ErrorRecord(
-        $exception,
-        $ErrorCode,
-        [System.Management.Automation.ErrorCategory]::NotSpecified,
-        $TargetObject
-    )
-}
-
-function ConvertTo-HelloIDPerson {
-    <#
-    .SYNOPSIS
-    Converts the list of Workers to a HelloID Person object
-
-    .DESCRIPTION
-    Converts the list of Workers to a HelloID Person object
+    Converts the worker objects to a raw data person object that can be imported into HelloID. Change this according to your ADP Workforce environment
 
     .PARAMETER Workers
     The list of Workers from ADP Workforce
@@ -285,50 +246,116 @@ function ConvertTo-HelloIDPerson {
         [PSObject]
         $Workers
     )
-
     process {
         [System.Collections.Generic.List[object]]$listWorkers = @()
-        [System.Collections.Generic.List[object]]$contracts = @()
-    
+
         foreach ($worker in $workers.workers) {
+            [System.Collections.Generic.List[object]]$contracts = @()
+
+            if ($null -ne $worker.businessCommunication){
+                if ($null -ne $worker.businessCommunication.landlines){
+                    $businessLandline = $worker.businessCommunication.landlines[0]
+                }
+                if ($null -ne $worker.businessCommunication.mobiles){
+                    $businessMobile = $worker.businessCommunication.mobiles[0]
+                }
+                if ($null -ne $worker.businessCommunication.emails){
+                    $businessEmail = $worker.businessCommunication.emails[0]
+                }
+            }
+
+            if ($null -ne $worker.person.communication){
+                if ($null -ne $worker.person.communication.landlines){
+                    $personalLandline = $worker.person.communication.landlines[0]
+                }
+                if ($null -ne $worker.person.communication.mobiles){
+                    $personalMobile = $worker.person.communication.mobiles[0]
+                }
+                if ($null -ne $worker.person.communcation.emails){
+                    $personalEmail = $worker.person.communcation.emails[0]
+                }
+            }
+
             $workerObj = [PSCustomObject]@{
-                FirstName = 'Anthony'
-                LastName = 'Albright'
-                Convention= 'A';
-                NickName = 'Ant'
-                ExteralId = $worker.associateOID
-                DisplayName = 'Antonie'
+                ExternalId = $worker.associateOID
+                DisplayName = $worker.person.legalName.formattedName
+                assocciateOID = $worker.associateOID
+                workerID = $worker.workerID
+                Person = $worker.person
+                BusinessCommunicationLandLine = $businessLandline
+                BusinessCommunicationMobile = $businessMobile
+                BusinessCommunicationEmail = $businessEmail
+                PersonalCommunicationLandLine = $personalLandline
+                PersonalCommunicationMobile = $personalMobile
+                PersonalCommunicationEmail = $personalEmail
+                WorkerDates = $worker.WorkerDates
+                WorkerStatus = $worker.workerStatus
                 Contracts = $contracts
-                Name = @{
-                    FamilyName = 'Albright'
-                }
             }
-    
-            foreach ($assignment in $worker.workAssignments){
-                for ($i = 0; $i -lt $worker.workAssignments.Length; $i++) {
-                    $contractObj = [PSCustomObject]@{
-                        WorkingHoursQuantatiy = $assignment.StandardHours.HoursQuantity
-                    }
-                }
-                $contracts.Add($contractObj)
-            }
-    
+
             $listWorkers.Add($workerObj)
+
+            if ($null -ne $worker.workAssignments){
+                foreach ($assignment in $worker.workAssignments){
+
+                    if ($null -ne $assignment.homeOrganizationalUnits){
+                        $HomeOrganizationlUnits = $assignment.homeOrganizationalUnits[0]
+                    }
+                    if ($null -ne $assignment.assignedOrganizationalUnits){
+                        $AssignedOrganizationalUnits = $assignment.assignedOrganizationalUnits[0]
+                    }
+                    if ($null -ne $assignment.reportsTo){
+                        $ReportsTo = $assignment.reportsTo[0]
+                    }
+                    if ($null -ne $assignment.assignedWorkLocations){
+                        $AssignedWorkLocation = $assignment.assignedWorkLocations[0]
+                    }
+
+                    $currentAssignment = [PSCustomObject]@{
+                        Assignment = $assignment
+                        HomeOrganizationlUnits = $HomeOrganizationlUnits
+                        AssignedOrganizationalUnits = $AssignedOrganizationalUnits
+                        ReportsTo = $ReportsTo
+                        AssignedWorkLocation = $AssignedWorkLocation
+                    }
+                    $contracts.Add($currentAssignment)
+                }
+            }
         }
-        $listWorkers | ConvertTo-Json
+        $listWorkers
     }
 }
 #EndRegion
 
 #Region Script
+<#
+.SYNOPSIS
+Retrieves the Workers and WorkerAssignments from ADP Workforce
+
+.DESCRIPTION
+Retrieves the Workers and WorkerAssignments from ADP Workforce
+
+.PARAMETER CientID
+The ClientID for the ADP Workforce environment. This will be provided by ADP
+
+.PARAMETER ClientSecret
+The ClientSecret for the ADP Workforce environment. This will be provided by ADP
+
+.PARAMETER Certificate
+The private key of the x.509 certificate that's used to generate a ClientID and ClientSecret and for activating the required API's.
+
+.PARAMETER ProxyServer
+The URL (or IP Address) to the ProxyServer in the network. Leave empty if no ProxyServer is being used
+
+.PARAMETER SecurityProtocol
+The encprytion protocol used when sending data across the network. The default is set to TLS1.2
+#>
 $SplatADPGetWorkers = @{
-    #ClientID = ''
-    #ClientSecret = ''
-    #Certificate = ''
-    #ProxyServer = "http://localhost:8888"
-    #UsePaging = $false
+    ClientID = ''
+    ClientSecret = ''
+    Certificate = ''
+    ProxyServer =  ''
+    SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 }
-Write-Verbose 'Starting Person import'
 Get-ADPWorkers @SplatADPGetWorkers
-Write-Verbose 'Person import completed'
 #EndRegion
