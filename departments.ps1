38f6cfc89ccbf377e406f515cb775212ce480c89
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2023-02-23 13:23:15 +0100

    Updated data response handling and logging

--- departments.ps1
+++ departments.ps1
@@ -1,7 +1,17 @@
+#####################################################
+# HelloID-Conn-Prov-Source-ADP-Workforce-Departments
+#
+# Version: 2.0.1
+#####################################################
+
 # Set TLS to accept TLS, TLS 1.1 and TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
-$VerbosePreference = "SilentlyContinue"
+# Set debug logging
+switch ($($c.isDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
 $InformationPreference = "Continue"
 $WarningPreference = "Continue"
 
@@ -9,37 +19,99 @@ $c = $configuration | ConvertFrom-Json
 
 $baseUrl = $($c.BaseUrl)
 $clientId = $($c.ClientID)
-$ClientSecret = $($c.ClientSecret)
+$clientSecret = $($c.ClientSecret)
 $certificatePath = $($c.CertificatePath)
 $certificatePassword = $($c.CertificatePassword)
 $proxyServer = $($c.ProxyServer)
 
-#Region functions
+Write-Information "Start department import: Base URL '$baseUrl', Proxy server '$proxyServer', Client ID '$clientId'"
+
+#region functions
+function Resolve-HTTPError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            MyCommand             = $ErrorObject.InvocationInfo.MyCommand
+            RequestUri            = $ErrorObject.TargetObject.RequestUri
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ''
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $httpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
+        }
+        elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            $httpErrorObj.ErrorMessage = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+        }
+        Write-Output $httpErrorObj
+    }
+}
+
+function Get-ErrorMessage {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $errorMessage = [PSCustomObject]@{
+            VerboseErrorMessage = $null
+            AuditErrorMessage   = $null
+        }
+
+        if ( $($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+            $httpErrorObject = Resolve-HTTPError -Error $ErrorObject
+
+            $errorMessage.VerboseErrorMessage = $httpErrorObject.ErrorMessage
+
+            $errorMessage.AuditErrorMessage = $httpErrorObject.ErrorMessage
+        }
+
+        # If error message empty, fall back on $ex.Exception.Message
+        if ([String]::IsNullOrEmpty($errorMessage.VerboseErrorMessage)) {
+            $errorMessage.VerboseErrorMessage = $ErrorObject.Exception.Message
+        }
+        if ([String]::IsNullOrEmpty($errorMessage.AuditErrorMessage)) {
+            $errorMessage.AuditErrorMessage = $ErrorObject.Exception.Message
+        }
+
+        Write-Output $errorMessage
+    }
+}
+
 function Get-ADPAccessToken {
     <#
-    .SYNOPSIS
-    Retrieves an AccessToken from the ADP API using the standard <Invoke-RestMethod> cmdlet
+.SYNOPSIS
+Retrieves an AccessToken from the ADP API using the standard <Invoke-RestMethod> cmdlet
 
-    .DESCRIPTION
-    The ADP Workforce API's uses OAuth for authentication\authorization.
-    Before data can be retrieved from the API's, an AccessToken has to obtained. The AccessToken is used for all consecutive calls to the ADP Workforce API's. 
-    Tokens only have access to a certain API scope. Default the scope is set to: 'worker-demographics organization-departments'. 
-    Data outside this scope from other API's cannot be retrieved
+.DESCRIPTION
+The ADP Workforce API's uses OAuth for authentication\authorization.
+Before data can be retrieved from the API's, an AccessToken has to obtained. The AccessToken is used for all consecutive calls to the ADP Workforce API's. 
+Tokens only have access to a certain API scope. Default the scope is set to: 'worker-demographics organization-departments'. 
+Data outside this scope from other API's cannot be retrieved
 
-    .PARAMETER ClientID
-    The ClientID for the ADP Workforce environment. This will be provided by ADP
+.PARAMETER ClientID
+The ClientID for the ADP Workforce environment. This will be provided by ADP
 
-    .PARAMETER ClientSecret
-    The ClientSecret for the ADP Workforce environment. This will be provided by ADP
+.PARAMETER ClientSecret
+The ClientSecret for the ADP Workforce environment. This will be provided by ADP
 
-    .PARAMETER Certificate
-    The [X509Certificate] object containing the *.pfx
+.PARAMETER Certificate
+The [X509Certificate] object containing the *.pfx
 
-    .EXAMPLE
-    $certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new("the path to the *.pfx file", "Password for the *.pfx certificate")
+.EXAMPLE
+$certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new("the path to the *.pfx file", "Password for the *.pfx certificate")
 
-    Get-ADPAccessToken -Client 'ADP_Provided_ClientID' -ClientSecret 'ADP_Provided_Secret' -Certifcate $certificate
-    #>
+Get-ADPAccessToken -Client 'ADP_Provided_ClientID' -ClientSecret 'ADP_Provided_Secret' -Certifcate $certificate
+#>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -48,7 +120,7 @@ function Get-ADPAccessToken {
 
         [Parameter(Mandatory)]
         [String]
-        $ClientSecret,
+        $clientSecret,
 
         [X509Certificate]
         $Certificate
@@ -60,7 +132,7 @@ function Get-ADPAccessToken {
 
     $body = @{
         client_id     = $ClientID
-        client_secret = $ClientSecret
+        client_secret = $clientSecret
         grant_type    = "client_credentials"
     }
 
@@ -72,43 +144,43 @@ function Get-ADPAccessToken {
             Body        = $body
             Certificate = $certificate
         }
-        Invoke-RestMethod @splatRestMethodParameters
+        Invoke-RestMethod @splatRestMethodParameters -verbose:$false
     }
     catch {
-        $PSCmdlet.ThrowTerminatingError($PSItem)
+        throw $PSItem
     }
 }
 
 function Invoke-ADPRestMethod {
     <#
-    .SYNOPSIS
-    Retrieves data from the ADP API's
+.SYNOPSIS
+Retrieves data from the ADP API's
 
-    .DESCRIPTION
-    Retrieves data from the ADP API's using the standard <Invoke-RestMethod> cmdlet
+.DESCRIPTION
+Retrieves data from the ADP API's using the standard <Invoke-RestMethod> cmdlet
 
-    .PARAMETER Url
-    The BaseUrl to the ADP Workforce environment. For example: https://test-api.adp.com
+.PARAMETER Url
+The BaseUrl to the ADP Workforce environment. For example: https://test-api.adp.com
 
-    .PARAMETER Method
-    The CRUD operation for the request. Valid HttpMethods inlcude: GET and POST. Note that the ADP API's needed for the connector will only support 'GET'
+.PARAMETER Method
+The CRUD operation for the request. Valid HttpMethods inlcude: GET and POST. Note that the ADP API's needed for the connector will only support 'GET'
 
-    .PARAMETER AccessToken
-    The AccessToken retrieved by the <Get-ADPAccessToken> function
+.PARAMETER AccessToken
+The AccessToken retrieved by the <Get-ADPAccessToken> function
 
-    .PARAMETER ProxyServer
-    The URL (or IP Address) to the ProxyServer in the network. Leave empty if no ProxyServer is being used
+.PARAMETER ProxyServer
+The URL (or IP Address) to the ProxyServer in the network. Leave empty if no ProxyServer is being used
 
-    .PARAMETER Certificate
-    The [X509Certificate] object containing the *.pfx
+.PARAMETER Certificate
+The [X509Certificate] object containing the *.pfx
 
-    .EXAMPLE
-    $certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new("the path to the *.pfx file", "Password for the *.pfx certificate")
+.EXAMPLE
+$certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new("the path to the *.pfx file", "Password for the *.pfx certificate")
 
-    Invoke-ADPRestMethod -Uri 'https://test-api.adp.com/hr/v2/worker-demographics' -Method 'GET' -AccessToken '0000-0000-0000-0000' -Certifcate $certificate
+Invoke-ADPRestMethod -Uri 'https://test-api.adp.com/hr/v2/worker-demographics' -Method 'GET' -AccessToken '0000-0000-0000-0000' -Certifcate $certificate
 
-    Returns the raw JSON data containing all workers from ADP Workforce
-    #>
+Returns the raw JSON data containing all workers from ADP Workforce
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
@@ -148,9 +220,8 @@ function Invoke-ADPRestMethod {
         $proxy = $ProxyServer
     }
 
-
     # Speficy the variables specific to certain endpoints
-    # $contentField = The field in the respons content that contains the actual data
+    # $contentField = The field in the response content that contains the actual data
     # $paging = A boolean specifying to user paging or not
     switch ($Url) {
         "https://api.eu.adp.com/hr/v2/worker-demographics" {
@@ -168,7 +239,7 @@ function Invoke-ADPRestMethod {
         if ($true -eq $paging) {
             # Fetch the data in smaller chunks, otherwise the API of ADP will return an error 500 Internal Server Error or an error 503 Server / Service unavailable
             $take = 100
-            $skip = 0   
+            $skip = 0
 
             do {
                 $result = $null
@@ -184,7 +255,10 @@ function Invoke-ADPRestMethod {
                     Certificate     = $Certificate
                 }
 
-                $dataset = Invoke-RestMethod @splatRestMethodParameters -ContentType "application/json;charset=utf-8"
+                $datasetJson = Invoke-WebRequest @splatRestMethodParameters -verbose:$false
+                $datasetCorrected = [Text.Encoding]::UTF8.GetString([Text.Encoding]::GetEncoding(28591).GetBytes($datasetJson.content))
+                $dataset = $datasetCorrected | ConvertFrom-Json
+
                 $result = $dataset.$contentField
                 if (-not [string]::IsNullOrEmpty($result)) {
                     $data.value.AddRange($result)
@@ -201,7 +275,11 @@ function Invoke-ADPRestMethod {
                 UseBasicParsing = $true
                 Certificate     = $Certificate
             }
-            $dataset = Invoke-RestMethod @splatRestMethodParameters
+        
+            $datasetJson = Invoke-WebRequest @splatRestMethodParameters -verbose:$false
+            $datasetCorrected = [Text.Encoding]::UTF8.GetString([Text.Encoding]::GetEncoding(28591).GetBytes($datasetJson.content))
+            $dataset = $datasetCorrected | ConvertFrom-Json
+
             $result = $dataset.$contentField
             if (-not [string]::IsNullOrEmpty($result)) {
                 $data.value.AddRange($result)
@@ -210,29 +288,32 @@ function Invoke-ADPRestMethod {
     }
     catch {
         $data.Value = $null
-        Write-Warning "Error querying data from ADP. URI: $Url. Error: $($_.Exception.Message) - $($_.ScriptStackTrace)"
-        Throw "A critical error occured. Please see the snapshot log for details..."
+        $ex = $PSItem
+        $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($($errorMessage.VerboseErrorMessage))"  
+        throw "Could not query data from ADP. URI: $($splatRestMethodParameters.Uri). Error Message: $($errorMessage.AuditErrorMessage)"
     }
 }
-#EndRegion
+#endregion functions
+
 
 # Create Access Token
 try {
     $certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($certificatePath, $certificatePassword)
     $accessToken = Get-ADPAccessToken -ClientID $clientId -ClientSecret $clientSecret -Certificate $certificate
 }
-catch [System.Net.WebException] {
-    $webEx = $PSItem
-    $errorObj = ($($webEx.ErrorDetails.Message) | ConvertFrom-Json).response
-    throw "Could not retrieve ADP AccessToken. Error: '$($errorObj.applicationCode.message)' Code: '$($errorObj.applicationCode.code)'"
-}
-catch [System.Exception] {
-    throw "Could not retrieve ADP AccessToken. Error: '$($_.Exception.Message)'"
+catch {
+    $ex = $PSItem
+    $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+    Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($($errorMessage.VerboseErrorMessage))"  
+    throw "Could not create ADP AccessToken. Error Message: $($errorMessage.AuditErrorMessage)"
 }
 
-# Query departments
+# Query Departments
 try {
-    Write-Verbose "Querying departments"
+    Write-Verbose "Querying Departments"
 
     $splatADPRestMethodParams = @{
         Url         = "$BaseUrl/core/v1/organization-departments"
@@ -245,6 +326,9 @@ try {
     $departments = [System.Collections.ArrayList]::new()
     Invoke-ADPRestMethod @splatADPRestMethodParams ([ref]$departments)
 
+    # Sort on ExternalId (to make sure the order is always the same)
+    $departments = $departments | Sort-Object -Property { $_.departmentCode.codeValue }
+
     $departments | Add-Member -MemberType NoteProperty -Name "customFields" -Value $null -Force
     $departments | ForEach-Object {
         if ($null -ne $_.auxilliaryFields) {
@@ -256,26 +340,36 @@ try {
             )
             $departmentAuxilliaryFields = $_ | Select-Object -Property $properties
             $_.customFields = $departmentAuxilliaryFields
+
+            # Remove unneccesary fields from  object (to avoid unneccesary large objects and confusion when mapping)
+            # Remove auxilliaryFields ,since the data is transformed into seperate object
+            $_.PSObject.Properties.Remove('auxilliaryFields')
         }
     }
 
-    Write-Information "Successfully queried departments. Result: $($departments.Count)"
+    Write-Information "Succesfully queried Departments. Result count: $($departments.count)"
 }
-catch [System.Net.WebException] {
-    $errorObj = ($($_.ErrorDetails.Message) | ConvertFrom-Json).response
-    throw "Could not retrieve ADP Departments. Error: '$($errorObj.applicationCode.message)' Code: '$($errorObj.applicationCode.code)'"
-}
-catch [System.Exception] {
-    throw "Could not retrieve ADP Departments. Error: '$($_.Exception.Message)'"
+catch {
+    $ex = $PSItem
+    $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+    Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($($errorMessage.VerboseErrorMessage))"  
+    throw "Could not query Departments. Error Message: $($errorMessage.AuditErrorMessage)"
 }
 
 try{
-    # Enhance department object and export
+    Write-Verbose 'Enhancing and exporting department objects to HelloID'
+
+    # Set counter to keep track of actual exported person objects
+    $exportedDepartments = 0
+
+    # Enhance department model with required properties
     $departments | Add-Member -MemberType NoteProperty -Name "ExternalId" -Value $null -Force
     $departments | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $null -Force
     $departments | Add-Member -MemberType NoteProperty -Name "Name" -Value $null -Force
     $departments | Add-Member -MemberType NoteProperty -Name "ManagerExternalId" -Value $null -Force
     $departments | Add-Member -MemberType NoteProperty -Name "ParentExternalId" -Value $null -Force
+    
     $departments | ForEach-Object {
         # Set required fields for HelloID
         $_.ExternalId = $_.departmentCode.codeValue
@@ -289,7 +383,17 @@ try{
         $department = $department.Replace("._", "__")
 
         Write-Output $department
+
+        # Updated counter to keep track of actual exported department objects
+        $exportedDepartments++
     }
-}catch{
-    throw $_
+    Write-Information "Succesfully enhanced and exported department objects to HelloID. Result count: $($exportedDepartments)"
+    Write-Information "Department import completed"
+}
+catch {
+    $ex = $PSItem
+    $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+    Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($($errorMessage.VerboseErrorMessage))"  
+    throw "Could not enhance and export department objects to HelloID. Error Message: $($errorMessage.AuditErrorMessage)"
 }
